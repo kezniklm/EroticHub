5e2dbf66a1d9b89f18214a9c36b135545adc6642
Matej Keznikl <xkezni01@vutbr.cz>
2024-11-26 13:22:13 +0100

    ADD: functions that does not fetch the password always

--- src/persistence/repositories/user.rs
+++ src/persistence/repositories/user.rs
@@ -8,6 +8,8 @@ pub trait UserRepoTrait: Debug {
     async fn create_user(&self, user: User) -> anyhow::Result<User>;
     async fn get_user_by_id(&self, user_id: i32) -> anyhow::Result<Option<User>>;
     async fn get_user_by_username(&self, username: &str) -> anyhow::Result<Option<User>>;
+    async fn get_user_by_id_full(&self, user_id: i32) -> anyhow::Result<Option<User>>;
+    async fn get_user_by_username_full(&self, username: &str) -> anyhow::Result<Option<User>>;
     async fn update_user(&self, user: User) -> anyhow::Result<Option<User>>;
     async fn delete_user(&self, user_id: i32) -> anyhow::Result<bool>;
     async fn list_users(&self) -> anyhow::Result<Vec<User>>;
@@ -51,7 +53,7 @@ impl UserRepoTrait for UserRepository {
         let user = sqlx::query_as!(
             User,
             r#"
-            SELECT id, username, password_hash, email, profile_picture_path, artist_id, paying_member_id
+            SELECT id, username, NULL as password_hash, email, profile_picture_path, artist_id, paying_member_id
             FROM user_table
             WHERE id = $1
             "#,
@@ -64,6 +66,38 @@ impl UserRepoTrait for UserRepository {
     }
 
     async fn get_user_by_username(&self, username: &str) -> anyhow::Result<Option<User>> {
+        let user = sqlx::query_as!(
+            User,
+            r#"
+            SELECT id, username, NULL as password_hash, email, profile_picture_path, artist_id, paying_member_id
+            FROM user_table
+            WHERE username = $1
+            "#,
+            username
+        )
+            .fetch_optional(&self.pool)
+            .await?;
+
+        Ok(user)
+    }
+
+    async fn get_user_by_id_full(&self, user_id: i32) -> anyhow::Result<Option<User>> {
+        let user = sqlx::query_as!(
+            User,
+            r#"
+            SELECT id, username, password_hash, email, profile_picture_path, artist_id, paying_member_id
+            FROM user_table
+            WHERE id = $1
+            "#,
+            user_id
+        )
+            .fetch_optional(&self.pool)
+            .await?;
+
+        Ok(user)
+    }
+
+    async fn get_user_by_username_full(&self, username: &str) -> anyhow::Result<Option<User>> {
         let user = sqlx::query_as!(
             User,
             r#"
