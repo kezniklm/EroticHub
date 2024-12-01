450980aee7ba1b89a04d745ae279dce4e8f95059
Matej Keznikl <xkezni01@vutbr.cz>
2024-12-01 15:27:11 +0100

    ADD: repository tests for UserRepository

--- src/persistence/repositories/user.rs
+++ src/persistence/repositories/user.rs
@@ -171,3 +171,225 @@ impl UserRepositoryTrait for UserRepository {
         Ok(users)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::common::tests::setup::AsyncContext;
+    use crate::persistence::entities::user::User;
+    use crate::persistence::repositories::user::{UserRepository, UserRepositoryTrait};
+    use test_context::test_context;
+
+    #[test_context(AsyncContext)]
+    #[tokio::test]
+    async fn test_create_user(context: &AsyncContext) -> anyhow::Result<()> {
+        let user_repo = UserRepository::new(context.pg_pool.clone());
+
+        let new_user = User {
+            id: 0, // id will be auto-generated
+            username: "test_user".to_string(),
+            password_hash: Some("hashed_password".to_string()),
+            email: "test_user@example.com".to_string(),
+            profile_picture_path: Some("path/to/pic.jpg".to_string()),
+            artist_id: None,
+            paying_member_id: None,
+        };
+
+        let created_user = user_repo.create_user(new_user.clone()).await?;
+
+        assert_eq!(created_user.username, new_user.username);
+        assert_eq!(created_user.email, new_user.email);
+        assert!(created_user.id > 0);
+
+        Ok(())
+    }
+
+    #[test_context(AsyncContext)]
+    #[tokio::test]
+    async fn test_get_user_by_id(context: &AsyncContext) -> anyhow::Result<()> {
+        let user_repo = UserRepository::new(context.pg_pool.clone());
+
+        let new_user = User {
+            id: 0, // id will be auto-generated
+            username: "get_user_test".to_string(),
+            password_hash: Some("hashed_password".to_string()),
+            email: "get_user_test@example.com".to_string(),
+            profile_picture_path: Some("path/to/pic.jpg".to_string()),
+            artist_id: None,
+            paying_member_id: None,
+        };
+
+        let created_user = user_repo.create_user(new_user).await?;
+
+        let fetched_user = user_repo.get_user_by_id(created_user.id).await?;
+        assert!(fetched_user.is_some());
+        assert_eq!(fetched_user.unwrap().id, created_user.id);
+
+        Ok(())
+    }
+
+    #[test_context(AsyncContext)]
+    #[tokio::test]
+    async fn test_get_user_by_username(context: &AsyncContext) -> anyhow::Result<()> {
+        let user_repo = UserRepository::new(context.pg_pool.clone());
+
+        let new_user = User {
+            id: 0, // id will be auto-generated
+            username: "get_user_by_username_test".to_string(),
+            password_hash: Some("hashed_password".to_string()),
+            email: "get_user_by_username_test@example.com".to_string(),
+            profile_picture_path: Some("path/to/pic.jpg".to_string()),
+            artist_id: None,
+            paying_member_id: None,
+        };
+
+        let created_user = user_repo.create_user(new_user).await?;
+
+        let fetched_user = user_repo
+            .get_user_by_username(&created_user.username)
+            .await?;
+        assert!(fetched_user.is_some());
+        assert_eq!(fetched_user.unwrap().username, created_user.username);
+
+        Ok(())
+    }
+
+    #[test_context(AsyncContext)]
+    #[tokio::test]
+    async fn test_update_user(context: &AsyncContext) -> anyhow::Result<()> {
+        let user_repo = UserRepository::new(context.pg_pool.clone());
+
+        let new_user = User {
+            id: 0, // id will be auto-generated
+            username: "update_user_test".to_string(),
+            password_hash: Some("hashed_password".to_string()),
+            email: "update_user_test@example.com".to_string(),
+            profile_picture_path: Some("path/to/pic.jpg".to_string()),
+            artist_id: None,
+            paying_member_id: None,
+        };
+
+        let created_user = user_repo.create_user(new_user.clone()).await?;
+
+        let mut user_to_update = created_user;
+        user_to_update.email = "updated_email@example.com".to_string();
+
+        let updated_user = user_repo.update_user(user_to_update.clone()).await?;
+        assert!(updated_user.is_some());
+        assert_eq!(updated_user.unwrap().email, "updated_email@example.com");
+
+        Ok(())
+    }
+
+    #[test_context(AsyncContext)]
+    #[tokio::test]
+    async fn test_delete_user(context: &AsyncContext) -> anyhow::Result<()> {
+        let user_repo = UserRepository::new(context.pg_pool.clone());
+
+        let new_user = User {
+            id: 0, // id will be auto-generated
+            username: "delete_user_test".to_string(),
+            password_hash: Some("hashed_password".to_string()),
+            email: "delete_user_test@example.com".to_string(),
+            profile_picture_path: Some("path/to/pic.jpg".to_string()),
+            artist_id: None,
+            paying_member_id: None,
+        };
+        let created_user = user_repo.create_user(new_user).await?;
+
+        let is_deleted = user_repo.delete_user(created_user.id).await?;
+        assert!(is_deleted);
+
+        let fetched_user = user_repo.get_user_by_id(created_user.id).await?;
+        assert!(fetched_user.is_none());
+
+        Ok(())
+    }
+
+    #[test_context(AsyncContext)]
+    #[tokio::test]
+    async fn test_list_users(context: &AsyncContext) -> anyhow::Result<()> {
+        let user_repo = UserRepository::new(context.pg_pool.clone());
+
+        let user1 = User {
+            id: 0, // id will be auto-generated
+            username: "list_user_1".to_string(),
+            password_hash: Some("hashed_password".to_string()),
+            email: "list_user_1@example.com".to_string(),
+            profile_picture_path: Some("path/to/pic.jpg".to_string()),
+            artist_id: None,
+            paying_member_id: None,
+        };
+        let user2 = User {
+            id: 0, // id will be auto-generated
+            username: "list_user_2".to_string(),
+            password_hash: Some("hashed_password".to_string()),
+            email: "list_user_2@example.com".to_string(),
+            profile_picture_path: Some("path/to/pic.jpg".to_string()),
+            artist_id: None,
+            paying_member_id: None,
+        };
+        user_repo.create_user(user1).await?;
+        user_repo.create_user(user2).await?;
+
+        let users = user_repo.list_users().await?;
+        assert!(users.len() >= 2);
+
+        Ok(())
+    }
+
+    #[test_context(AsyncContext)]
+    #[tokio::test]
+    async fn test_get_user_by_id_full(context: &AsyncContext) -> anyhow::Result<()> {
+        let user_repo = UserRepository::new(context.pg_pool.clone());
+
+        let new_user = User {
+            id: 0, // id will be auto-generated
+            username: "get_user_by_id_full_test".to_string(),
+            password_hash: Some("hashed_password".to_string()),
+            email: "get_user_by_id_full_test@example.com".to_string(),
+            profile_picture_path: Some("path/to/pic.jpg".to_string()),
+            artist_id: None,
+            paying_member_id: None,
+        };
+
+        let created_user = user_repo.create_user(new_user).await?;
+
+        let fetched_user = user_repo.get_user_by_id_full(created_user.id).await?;
+        assert!(fetched_user.is_some());
+        assert_eq!(
+            fetched_user.unwrap().password_hash,
+            Some("hashed_password".to_string())
+        );
+
+        Ok(())
+    }
+
+    #[test_context(AsyncContext)]
+    #[tokio::test]
+    async fn test_get_user_by_username_full(context: &AsyncContext) -> anyhow::Result<()> {
+        let user_repo = UserRepository::new(context.pg_pool.clone());
+
+        let new_user = User {
+            id: 0, // id will be auto-generated
+            username: "get_user_by_username_full_test".to_string(),
+            password_hash: Some("hashed_password".to_string()),
+            email: "get_user_by_username_full_test@example.com".to_string(),
+            profile_picture_path: Some("path/to/pic.jpg".to_string()),
+            artist_id: None,
+            paying_member_id: None,
+        };
+
+        let created_user = user_repo.create_user(new_user).await?;
+
+        let fetched_user = user_repo
+            .get_user_by_username_full(&created_user.username)
+            .await?;
+        assert!(fetched_user.is_some());
+        assert_eq!(
+            fetched_user.unwrap().password_hash,
+            Some("hashed_password".to_string())
+        );
+
+        Ok(())
+    }
+}
