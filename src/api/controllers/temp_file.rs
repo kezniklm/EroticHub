662751033990f4ade61078ca0f34bb36029ebf6f
Martin Prochazka <240970@vutbr.cz>
2024-12-26 20:06:42 +0100

    feat: delete temp file endpoint refactor

--- src/api/controllers/temp_file.rs
+++ src/api/controllers/temp_file.rs
@@ -10,11 +10,17 @@ use actix_files::NamedFile;
 use actix_multipart::form::tempfile::TempFile;
 use actix_multipart::form::MultipartForm;
 use actix_web::http::StatusCode;
-use actix_web::web::{Data, Json, Path};
+use actix_web::web::{Data, Json, Path, Query};
 use actix_web::{Error, HttpResponse, Responder};
 use askama_actix::TemplateToResponse;
 use log::error;
 
+/// Creates new temporary video file
+///
+/// `POST /temp/video`
+///
+/// # Returns
+/// `VideoPreviewTemplate` - includes video player together with hidden input including temp_file_id
 pub async fn post_temp_video(
     MultipartForm(form): MultipartForm<VideoUploadForm>,
     temp_file_facade: Data<TempFileFacade>,
@@ -53,6 +59,12 @@ pub async fn post_temp_video(
     }
 }
 
+/// Creates new temporary file for thumbnail
+///
+/// `POST /temp/thumbnail`
+///
+/// # Returns
+/// `ThumbnailPreviewTemplate` - Thumbnail preview template together with hidden input with temp_file_id
 pub async fn post_temp_thumbnail(
     MultipartForm(form): MultipartForm<ThumbnailUploadForm>,
     temp_file_facade: Data<TempFileFacade>,
@@ -90,10 +102,17 @@ pub async fn post_temp_thumbnail(
     }
 }
 
+/// Get a temporary file for a preview
+///
+/// `GET /temp/{temp_file}`
+///
+/// # Returns
+/// Temporary file
 pub async fn get_temp_file(
     temp_file: Path<i32>,
     temp_file_facade: Data<TempFileFacade>,
 ) -> actix_web::Result<NamedFile> {
+    // TODO: CHECK PERMISSIONS
     match temp_file_facade
         .get_temp_file(temp_file.into_inner(), 1)
         .await
@@ -109,16 +128,27 @@ pub async fn get_temp_file(
     }
 }
 
-pub async fn delete_temp_video(
+/// Deletes temporary file
+///
+/// `DELETE /temp/{temp_file}`
+///
+/// # Query params
+/// - `GetFileInputTemplate` - decides which input template should be returned
+///
+/// # Returns
+/// Upload input template based on the request, so user can upload the item again
+pub async fn delete_temp_file(
     temp_file: Path<i32>,
+    Query(temp_file_type): Query<GetFileInputTemplate>,
     temp_file_facade: Data<TempFileFacade>,
     config: Data<Configuration>,
 ) -> impl Responder {
+    // TODO: Check permissions
     match temp_file_facade
         .delete_temp_file(temp_file.into_inner(), 1)
         .await
     {
-        Ok(_) => Ok(VideoUploadInputTemplate::new(config.into_inner()).to_response()),
+        Ok(_) => Ok(get_upload_template(temp_file_type, config)),
         Err(err) => {
             error!("Error while removing temporary video: {:#?}", err);
             Err(Error::from(actix_web::error::InternalError::new(
@@ -129,30 +159,27 @@ pub async fn delete_temp_video(
     }
 }
 
-pub async fn delete_temp_thumbnail(
-    temp_file: Path<i32>,
-    temp_file_facade: Data<TempFileFacade>,
-    config: Data<Configuration>,
-) -> impl Responder {
-    match temp_file_facade
-        .delete_temp_file(temp_file.into_inner(), 1)
-        .await
-    {
-        Ok(_) => Ok(ThumbnailUploadInputTemplate::new(config.into_inner()).to_response()),
-        Err(err) => {
-            error!("Error while removing temporary video: {:#?}", err);
-            Err(Error::from(actix_web::error::InternalError::new(
-                "Deletion of temporary video file failed",
-                StatusCode::INTERNAL_SERVER_ERROR,
-            )))
-        }
-    }
-}
-
+/// Returns templates for file inputs. Can be used in case when user want's to re-upload the video,
+/// but the video is no longer temporary file (video is already saved).
+///
+/// `GET /temp/template`
+///
+/// # Body params
+/// - `Json<GetFileInputTemplate>` - decides which input template should be returned
+///
+/// # Returns
+/// Template HTML based on the request.
 pub async fn get_input_template(
     Json(temp_file_type): Json<GetFileInputTemplate>,
     config: Data<Configuration>,
 ) -> impl Responder {
+    get_upload_template(temp_file_type, config)
+}
+
+fn get_upload_template(
+    temp_file_type: GetFileInputTemplate,
+    config: Data<Configuration>,
+) -> HttpResponse {
     match temp_file_type.input_type {
         TempFileInput::Video => VideoUploadInputTemplate::new(config.into_inner()).to_response(),
         TempFileInput::Thumbnail => {
--- src/api/routes/temp_file.rs
+++ src/api/routes/temp_file.rs
@@ -1,6 +1,5 @@
 use crate::api::controllers::temp_file::{
-    delete_temp_thumbnail, delete_temp_video, get_input_template, get_temp_file,
-    post_temp_thumbnail, post_temp_video,
+    delete_temp_file, get_input_template, get_temp_file, post_temp_thumbnail, post_temp_video,
 };
 use actix_web::web;
 
@@ -9,11 +8,7 @@ pub fn temp_file_routes(cfg: &mut web::ServiceConfig) {
         web::scope("/temp")
             .route("/{temp_file}", web::get().to(get_temp_file))
             .route("/video", web::post().to(post_temp_video))
-            .route("/video/{temp_file}", web::delete().to(delete_temp_video))
-            .route(
-                "/thumbnail/{temp_file}",
-                web::delete().to(delete_temp_thumbnail),
-            )
+            .route("/{temp_file}", web::delete().to(delete_temp_file))
             .route("/thumbnail", web::post().to(post_temp_thumbnail))
             .route("/template", web::get().to(get_input_template)),
     );
