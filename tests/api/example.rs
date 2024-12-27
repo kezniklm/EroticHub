c1e7ac89256d4905d05faf76bd34739e11bf2ce3
Martin Prochazka <240970@vutbr.cz>
2024-12-27 13:54:14 +0100

    fix: formatting & removed prints

--- tests/api/example.rs
+++ tests/api/example.rs
@@ -7,19 +7,16 @@ use test_context::test_context;
 #[test_context(AsyncContext)]
 #[actix_web::test]
 async fn test01(ctx: &mut AsyncContext) {
-    println!("test01 start");
     let app = test::init_service(App::new().configure(ctx.configure_app())).await;
     let req = test::TestRequest::default().uri("/user");
 
     let response = req.send_request(&app).await;
     assert_eq!(response.status(), StatusCode::OK);
-    println!("test01 end");
 }
 
 #[test_context(AsyncContext)]
 #[actix_web::test]
 async fn test02(ctx: &mut AsyncContext) {
-    println!("test02 start");
     let app = test::init_service(App::new().configure(ctx.configure_app())).await;
 
     let (header, body) = files::VIDEO1.get_multipart_builder("file", "video/mp4");
@@ -31,5 +28,4 @@ async fn test02(ctx: &mut AsyncContext) {
 
     let response = req.send_request(&app).await;
     assert_eq!(response.status(), StatusCode::OK);
-    println!("test02 end");
 }
--- tests/common/files.rs
+++ tests/common/files.rs
@@ -1,8 +1,12 @@
-use std::path::{Path, PathBuf};
 use actix_multipart_test::MultiPartFormDataBuilder;
+use std::path::{Path, PathBuf};
 
 const TEST_DATA_PATH: &str = "tests/test_data";
-pub const VIDEO1: TestFile = TestFile { dir_path: TEST_DATA_PATH, file_name: "video1.mp4", test_file_type: TestFileType::Video };
+pub const VIDEO1: TestFile = TestFile {
+    dir_path: TEST_DATA_PATH,
+    file_name: "video1.mp4",
+    test_file_type: TestFileType::Video,
+};
 
 pub struct TestFile {
     dir_path: &'static str,
@@ -12,13 +16,26 @@ pub struct TestFile {
 
 impl TestFile {
     fn get_path_to_file(&self) -> PathBuf {
-        Path::new(&self.dir_path).join(self.test_file_type.get_folder_name()).join(self.file_name)
+        Path::new(&self.dir_path)
+            .join(self.test_file_type.get_folder_name())
+            .join(self.file_name)
     }
 
-    pub fn get_multipart_builder(&self, property_name: impl Into<String>, content_type: impl Into<String>) -> ((String, String), Vec<u8>) {
+    pub fn get_multipart_builder(
+        &self,
+        property_name: impl Into<String>,
+        content_type: impl Into<String>,
+    ) -> ((String, String), Vec<u8>) {
         let mut builder = MultiPartFormDataBuilder::new();
 
-        builder.with_file(self.get_path_to_file(), property_name, content_type, self.file_name).build()
+        builder
+            .with_file(
+                self.get_path_to_file(),
+                property_name,
+                content_type,
+                self.file_name,
+            )
+            .build()
     }
 }
 
