6fef525ae74adc7bfad396407b1da736baed3664
Matej Keznikl <xkezni01@vutbr.cz>
2025-01-13 19:03:28 +0100

    REFACTOR: user related structs to user.rs

--- src/business/models/mod.rs
+++ src/business/models/mod.rs
@@ -2,7 +2,5 @@ pub mod artist_detail;
 pub mod comment;
 pub mod error;
 pub mod stream;
-pub mod user_detail;
-pub mod user_list;
-pub mod user_register;
+pub mod user;
 pub mod video;
---
+++ src/business/models/user.rs
@@ -0,0 +1,60 @@
+use actix_multipart::form::tempfile::TempFile;
+use actix_multipart::form::text::Text;
+use actix_multipart::form::MultipartForm;
+use serde::{Deserialize, Serialize};
+use validator::Validate;
+
+#[derive(Clone, Debug)]
+pub struct UserDetail {
+    pub id: i32,
+    pub username: String,
+    pub email: String,
+    pub profile_picture_path: Option<String>,
+    pub artist_id: Option<i32>,
+    pub paying_member_id: Option<i32>,
+}
+
+#[derive(Clone, Debug, Deserialize, Validate)]
+pub struct UserRegister {
+    #[validate(length(min = 3, max = 12))]
+    pub username: String,
+    #[validate(length(min = 8, max = 128))]
+    pub password: String,
+    #[validate(must_match(other = "password"))]
+    pub password2: String,
+    #[validate(email)]
+    pub email: String,
+}
+
+#[derive(MultipartForm)]
+pub struct UserRegisterMultipart {
+    pub username: Text<String>,
+    pub password: Text<String>,
+    pub password2: Text<String>,
+    pub email: Text<String>,
+    #[multipart(limit = "10MB")]
+    pub profile_picture: Option<TempFile>,
+}
+
+#[derive(Clone, Debug, Deserialize)]
+pub struct UserLogin {
+    pub username: String,
+    pub password: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct UserSessionData {
+    pub profile_picture_path: Option<String>,
+}
+
+#[derive(Deserialize)]
+pub struct UsernameQuery {
+    pub username: String,
+    pub target_element: String,
+}
+
+#[derive(Deserialize)]
+pub struct EmailQuery {
+    pub email: String,
+    pub target_element: String,
+}
