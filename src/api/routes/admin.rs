caf6cd6b6f6f77b49b3dcd4134902e8b2ec9930d
Marek Vesely <xvesely4@fi.muni.cz>
2025-01-25 14:50:21 +0100

    fix: anchor tags navigation

--- src/api/routes/admin.rs
+++ src/api/routes/admin.rs
@@ -1,7 +1,11 @@
 use actix_web::web;
 
-use crate::api::controllers::admin::get_admin_section;
+use crate::api::controllers::admin::{get_admin_memberships, get_admin_section};
 
 pub fn admin_routes(cfg: &mut web::ServiceConfig) {
-    cfg.service(web::scope("/admin").route("", web::get().to(get_admin_section)));
+    cfg.service(
+        web::scope("/admin")
+            .route("", web::get().to(get_admin_section))
+            .route("/memberships", web::get().to(get_admin_memberships)),
+    );
 }
