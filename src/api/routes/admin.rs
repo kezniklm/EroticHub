2b5285b3467e71079ee77b02007f0080e4fc0630
Marek Vesely <xvesely4@fi.muni.cz>
2025-01-25 14:35:41 +0100

    feat: add admin section

---
+++ src/api/routes/admin.rs
@@ -0,0 +1,7 @@
+use actix_web::web;
+
+use crate::api::controllers::admin::get_admin_section;
+
+pub fn admin_routes(cfg: &mut web::ServiceConfig) {
+    cfg.service(web::scope("/admin").route("", web::get().to(get_admin_section)));
+}
