9b50f2c0ebff09fb556cafdd3068d3ef27a96a30
Martin Prochazka <240970@vutbr.cz>
2025-01-27 20:23:47 +0100

    feat: don't show camera icon to not artist users

--- src/api/templates/template.rs
+++ src/api/templates/template.rs
@@ -1,4 +1,5 @@
 use crate::api::extractors::htmx_extractor::HtmxRequest;
+use crate::api::extractors::permissions_extractor::IsRole;
 use crate::business::models::user::UserSessionData;
 use actix_session::Session;
 use askama::Template;
@@ -9,12 +10,14 @@ pub struct BaseTemplate<T: Template> {
     child_template: T,
     htmx_request: HtmxRequest,
     user_session_data: Option<UserSessionData>,
+    session: Session,
 }
 
 impl<T: Template> BaseTemplate<T> {
     pub fn wrap(htmx_request: HtmxRequest, session: Session, child_template: T) -> Self {
         Self {
             htmx_request,
+            session: session.clone(),
             user_session_data: session
                 .get::<UserSessionData>("user_session_data")
                 .unwrap_or(None),
